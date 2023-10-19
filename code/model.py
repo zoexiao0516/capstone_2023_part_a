9a65906b75024c6089f584e6c2f9a3866c939c88
Shreemayi <shreemayisonti7@gmail.com>
2023-10-19 18:08:11 -0400

    Added model code

---
+++ code/model.py
@@ -0,0 +1,175 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils import data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from tensorboardX import SummaryWriter
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+NUM_EPOCHS = 90
+BATCH_SIZE = 128
+MOMENTUM = 0.9
+LR_DECAY = 0.0005
+LR_INIT = 0.01
+IMAGE_DIM = 227
+NUM_CLASSES = 50 # Should be 1000 on the whole
+DEVICE_IDS = [0, 1, 2, 3]
+
+
+# /vast/work/public/ml-datasets/imagenet
+INPUT_ROOT_DIR = '/scratch/work/public/imagenet/'
+TRAIN_IMG_DIR = '/scratch/work/public/imagenet/imagenet-train.sqf'
+OUTPUT_DIR = '/scratch/yx1750/capstone2023/'
+LOG_DIR = OUTPUT_DIR + '/tblogs'
+CHECKPOINT_DIR = OUTPUT_DIR + '/models'
+os.makedirs(CHECKPOINT_DIR, exist_ok=True)
+
+
+class AlexNet(nn.Module):
+    def __init__(self, num_classes=1000):
+        # Conv layers
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(96, 256, 5, padding=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(256, 384, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(384, 256, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+
+        # Fully connected layers
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=(256 * 6 * 6), out_features=4096),
+            nn.ReLU(),
+            nn.Dropout(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Linear(in_features=4096, out_features=num_classes),
+        )
+        self.init_bias()
+
+    def init_bias(self):
+        for layer in self.net:
+            if isinstance(layer, nn.Conv2d):
+                nn.init.normal_(layer.weight, mean=0, std=0.01)
+                nn.init.constant_(layer.bias, 0)
+        nn.init.constant_(self.net[4].bias, 1)
+        nn.init.constant_(self.net[10].bias, 1)
+        nn.init.constant_(self.net[12].bias, 1)
+
+    def forward(self, x):
+        x = self.net(x)
+        x = x.view(-1, 256 * 6 * 6)  # flattening
+        return self.classifier(x)
+
+
+if __name__ == '__main__':
+
+    seed = torch.initial_seed()
+
+    tbwriter = SummaryWriter(log_dir=LOG_DIR)
+
+    alexnet = AlexNet(num_classes=NUM_CLASSES).to(device)
+
+    alexnet = torch.nn.parallel.DataParallel(alexnet, device_ids=DEVICE_IDS)
+    print(alexnet)
+    print('AlexNet created')
+
+    dataset = datasets.ImageFolder(TRAIN_IMG_DIR, transforms.Compose([
+        transforms.CenterCrop(IMAGE_DIM),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]))
+
+    print('Dataset created')
+    dataloader = data.DataLoader(
+        dataset,
+        shuffle=True,
+        pin_memory=True,
+        num_workers=8,
+        drop_last=True,
+        batch_size=BATCH_SIZE)
+    print('Dataloader created')
+
+    optimizer = optim.Adam(params=alexnet.parameters(), lr=0.0001)
+    print('Optimizer created')
+
+    lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
+    print('LR Scheduler created')
+
+    print('Starting training...')
+    total_steps = 1
+    for epoch in range(NUM_EPOCHS):
+        for imgs, classes in dataloader:
+            imgs, classes = imgs.to(device), classes.to(device)
+            #             print(imgs[0], classes[0])
+
+            # calculate the loss
+            output = alexnet(imgs)
+            #             print(output)
+            loss = F.cross_entropy(output, classes)
+
+            #             print(loss.item(), "Loss")
+
+            # update the parameters
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            lr_scheduler.step()
+
+            # log the information and add to tensorboard
+            if total_steps % 10 == 0:
+                with torch.no_grad():
+                    _, preds = torch.max(output, 1)
+                    accuracy = torch.sum(preds == classes)
+
+                    print('Epoch: {} \tStep: {} \tLoss: {:.4f} \tAcc: {}'
+                          .format(epoch + 1, total_steps, loss.item(), accuracy.item()))
+                    tbwriter.add_scalar('loss', loss.item(), total_steps)
+                    tbwriter.add_scalar('accuracy', accuracy.item(), total_steps)
+
+            # print out gradient values and parameter average values
+            if total_steps % 100 == 0:
+                with torch.no_grad():
+                    print('*' * 10)
+                    for name, parameter in alexnet.named_parameters():
+                        if parameter.grad is not None:
+                            avg_grad = torch.mean(parameter.grad)
+                            print('\t{} - grad_avg: {}'.format(name, avg_grad))
+                            tbwriter.add_scalar('grad_avg/{}'.format(name), avg_grad.item(), total_steps)
+                            tbwriter.add_histogram('grad/{}'.format(name),
+                                                   parameter.grad.cpu().numpy(), total_steps)
+                        if parameter.data is not None:
+                            avg_weight = torch.mean(parameter.data)
+                            print('\t{} - param_avg: {}'.format(name, avg_weight))
+                            tbwriter.add_histogram('weight/{}'.format(name),
+                                                   parameter.data.cpu().numpy(), total_steps)
+                            tbwriter.add_scalar('weight_avg/{}'.format(name), avg_weight.item(), total_steps)
+
+            total_steps += 1
+
+        # save checkpoints
+        checkpoint_path = os.path.join(CHECKPOINT_DIR, 'alexnet_states_e{}.pkl'.format(epoch + 1))
+        state = {
+            'epoch': epoch,
+            'total_steps': total_steps,
+            'optimizer': optimizer.state_dict(),
+            'model': alexnet.state_dict(),
+            'seed': seed,
+        }
+        torch.save(state, checkpoint_path)
