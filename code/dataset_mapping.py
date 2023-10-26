a56f8ed5be859a00a74a1395b79107c76b6e7963
Shreemayi <shreemayisonti7@gmail.com>
2023-10-26 19:46:34 -0400

    created_dataset_mapping

---
+++ code/dataset_mapping.py
@@ -0,0 +1,32 @@
+from torch.utils import data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+
+
+TRAIN_IMG_DIR = ''
+IMAGE_DIM = 227
+
+
+dataset = datasets.ImageFolder(TRAIN_IMG_DIR, transforms.Compose([
+        transforms.CenterCrop(IMAGE_DIM),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]))
+
+print('Dataset created')
+dataloader = data.DataLoader(
+    dataset,
+    shuffle=True,
+    pin_memory=True,
+    drop_last=True,
+    batch_size=2)
+print('Dataloader created')
+
+class_to_idx = dataset.class_to_idx
+idx_to_class = {v: k for k, v in class_to_idx.items()}
+
+# Example usage of class to label mapping
+print("Class to Label Mapping:")
+for class_name, label in class_to_idx.items():
+    print(f"{class_name}: {label}")
